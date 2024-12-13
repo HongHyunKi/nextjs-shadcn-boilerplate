12b48ea46138499a663d88832f1c71a6e6e752c4
hyunki <hk7286@naver.com>
2024-12-13 15:18:59 +0900

    update: install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Button } from '@/components/ui/button';
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -8,7 +9,8 @@ export default function Home() {
   return (
     <div className="p-4">
       <h1>hello world</h1>
-      <h1 className="text-3xl font-bold underline"> TailWind ! </h1>
+      {/*<h1 className="text-3xl font-bold underline"> TailWind ! </h1>*/}
+      <Button> Hello shadcn </Button>
     </div>
   );
 }
