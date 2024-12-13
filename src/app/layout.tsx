5ec7ebc9d351faaa6f57a85591a02cb8600aebc9
hyunki <hk7286@naver.com>
2024-12-13 15:54:17 +0900

    update: 레이아웃 틀 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
-import '../config/globals.css';
+import '../assets/css/globals.css';
+import { Footer } from '@/components/layouts/Footer';
+import { Header } from '@/components/layouts/Header';
 
 export const metadata: Metadata = {
   title: {
@@ -15,8 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="h-full scroll-my-20 scroll-smooth">
+      <body className="font-pretendard flex min-h-screen flex-col">
+        <Header />
+        <main className="mt-[64px] flex flex-1 flex-col mx-auto w-full max-w-[1200px] p-4">
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from 'next';
-import { Button } from '@/components/ui/button';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
 
-export default function Home() {
+export default async function Home() {
   return (
-    <div className="p-4">
+    <div>
       <h1>hello world</h1>
-      {/*<h1 className="text-3xl font-bold underline"> TailWind ! </h1>*/}
-      <Button> Hello shadcn </Button>
     </div>
   );
 }
