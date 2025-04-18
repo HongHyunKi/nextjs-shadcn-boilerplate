bde41479f619bfb18072a335192e5dee5943f8a9
hyunki <hk7286@naver.com>
2025-04-18 17:49:27 +0900

    fix: 사이드바 제거

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div>
-      <div className="flex-1 rounded-xl bg-muted/50 md:min-h-min">
-        Create Some Page!
-      </div>
-    </div>
-  );
-}
