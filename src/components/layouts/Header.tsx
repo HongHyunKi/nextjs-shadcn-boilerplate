cfd37c25e187bb41b939f2265a426dd6a3f2a1c9
hyunki <hk7286@naver.com>
2025-04-24 15:35:39 +0900

    update: 스크롤 투명도 적용

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -5,12 +5,30 @@ import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { ModeToggle } from '@/components/theme/ModeToggle';
 import { NAV_LIST } from '@/constants';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
   const pathname = usePathname();
+  const [isScrolled, setIsScrolled] = useState(false); // 배경색 투명 조건
+
+  useEffect(() => {
+    const onScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
   return (
-    <nav className="fixed z-40 flex w-full flex-col items-center justify-center border-b bg-white shadow-sm dark:bg-black print:hidden">
+    <nav
+      className={cn(
+        'fixed z-40 flex w-full flex-col items-center justify-center border-b bg-white shadow-sm dark:bg-black print:hidden',
+        isScrolled
+          ? 'bg-white/80 backdrop-blur-sm dark:bg-black/80'
+          : 'bg-white dark:bg-black'
+      )}
+    >
       <div className="flex h-[var(--header-height)] w-full max-w-[var(--container-max-width)] items-center justify-between px-[var(--container-px)]">
         <div className="flex items-center font-medium">
           {NAV_LIST.map((navItem) => (
@@ -19,7 +37,7 @@ export const Header = () => {
               key={navItem.name}
               className={cn(
                 'rounded-full px-4 py-1 text-center text-sm transition-colors hover:text-primary',
-                pathname?.startsWith(navItem.href)
+                pathname === navItem.href
                   ? 'bg-muted font-medium text-primary'
                   : 'text-muted-foreground'
               )}
